87de32cef367ea0f99ba9e7faa0ba8dc4e9252d3
danish4855281-prog <danish4855281@gmail.com>
2025-12-07 23:54:19 +0500

    Create Task 1.cpp

---
+++ Lab-01/Task 1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout<<" Welcome to C++ lab!"<<endl;
+cout<<" you will learn basic output today."<<endl;
+cout<<" Let's start with simple message."<<endl;
+
+return 0;
+}
