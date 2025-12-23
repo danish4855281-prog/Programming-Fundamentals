8e22805e50c60add5717715c0c8baec4a3034c8f
Danish Iqbal <danish4855281@gmail.com>
2025-12-23 15:26:53 +0500

    Add files via upload

---
+++ Lab-02-Escape Sequences/Task 1.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	cout<<"Item\tPrice\tQuality\nPen\t$1\t10\nPenclil\t$2\t5\nEraser\t$9.5\t20"<<endl;
+	
+	return 0;
+}
---
+++ Lab-02-Escape Sequences/Task 2.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	cout<<"She said,\"Let's learn C++!\"\nTo start, save your file in C:\\C++-Projects\\ Lab1\nGood Luck!"<<endl;
+	
+	return 0;
+}
